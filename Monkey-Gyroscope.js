f354a9b1df277b606bce6ec16497c397d012a58f
heronhe <anhuiheliang@sina.com>
2017-09-25 17:51:40 +0800

    陀螺仪

---
+++ Monkey-Gyroscope.js
@@ -0,0 +1,62 @@
+
+var MK_Gyroscope = (function () {
+	
+	var initStatus = false,
+		IOS_Ratio = 45,
+		yaoyaoNum = 0,
+		yaoyaoTriggerNum = 8, //摇摇触发次数
+		yaoyaoTriggerVal = 4, //摇摇触发值
+		//ios终端
+		isIos = (function(){
+			var u = navigator.userAgent;
+		    return !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+		})();
+
+	function init(){
+		if(initStatus)
+			return;
+
+		initStatus = true;
+
+		function motionHandler(event){  
+	        var rotationRate = event.rotationRate;
+
+	        var alpha = rotationRate.alpha / (isIos ? IOS_Ratio : 1),
+	        	beta = rotationRate.beta / (isIos ? IOS_Ratio : 1),
+	        	gamma = rotationRate.gamma / (isIos ? IOS_Ratio : 1);
+
+	        if(typeof MK_Gyroscope.motion === 'function'){
+	        	MK_Gyroscope.motion({
+	        		motionFB: alpha,
+	        		motionRL: gamma,
+	        		motionRT: beta
+	        	});
+	        }
+
+	        //摇一摇
+	        if(typeof MK_Gyroscope.yaoyao === 'function'){
+	        	if(Math.abs(gamma) > yaoyaoTriggerVal){
+	        		if(++yaoyaoNum >= yaoyaoTriggerNum){
+	        			MK_Gyroscope.yaoyao();
+	        			yaoyaoNum = 0;
+	        		}
+	        	}
+	        	
+	        }
+	    }  
+
+		if (window.DeviceMotionEvent){  
+		    window.addEventListener("devicemotion", motionHandler, false);  
+		}
+	}
+
+	var MK_Gyroscope = {
+		init: init,
+		rotation: null,
+		yaoyao: null,
+		isIos: isIos
+	};
+
+	return MK_Gyroscope;
+
+})();
