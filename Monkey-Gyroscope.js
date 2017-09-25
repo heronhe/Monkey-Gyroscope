79d98f388bd484ac5e3a9d02cc6499bbd7ff4ca2
heronhe <anhuiheliang@sina.com>
2017-09-25 18:12:09 +0800

    陀螺仪 clear

--- Monkey-Gyroscope.js
+++ Monkey-Gyroscope.js
@@ -12,39 +12,41 @@ var MK_Gyroscope = (function () {
 		    return !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
 		})();
 
+	//motion
+	function motionHandler(event){  
+	    var rotationRate = event.rotationRate;
+
+	    var alpha = rotationRate.alpha / (isIos ? IOS_Ratio : 1),
+	    	beta = rotationRate.beta / (isIos ? IOS_Ratio : 1),
+	    	gamma = rotationRate.gamma / (isIos ? IOS_Ratio : 1);
+
+	    if(typeof MK_Gyroscope.motion === 'function'){
+	    	MK_Gyroscope.motion({
+	    		motionFB: alpha,
+	    		motionRL: gamma,
+	    		motionRT: beta
+	    	});
+	    }
+
+	    //摇一摇
+	    if(typeof MK_Gyroscope.yaoyao === 'function'){
+	    	if(Math.abs(gamma) > yaoyaoTriggerVal){
+	    		if(++yaoyaoNum >= yaoyaoTriggerNum){
+	    			MK_Gyroscope.yaoyao();
+	    			yaoyaoNum = 0;
+	    		}
+	    	}
+	    	
+	    }
+	}
+
+	//初始化
 	function init(){
 		if(initStatus)
 			return;
 
 		initStatus = true;
-
-		function motionHandler(event){  
-	        var rotationRate = event.rotationRate;
-
-	        var alpha = rotationRate.alpha / (isIos ? IOS_Ratio : 1),
-	        	beta = rotationRate.beta / (isIos ? IOS_Ratio : 1),
-	        	gamma = rotationRate.gamma / (isIos ? IOS_Ratio : 1);
-
-	        if(typeof MK_Gyroscope.motion === 'function'){
-	        	MK_Gyroscope.motion({
-	        		motionFB: alpha,
-	        		motionRL: gamma,
-	        		motionRT: beta
-	        	});
-	        }
-
-	        //摇一摇
-	        if(typeof MK_Gyroscope.yaoyao === 'function'){
-	        	if(Math.abs(gamma) > yaoyaoTriggerVal){
-	        		if(++yaoyaoNum >= yaoyaoTriggerNum){
-	        			MK_Gyroscope.yaoyao();
-	        			yaoyaoNum = 0;
-	        		}
-	        	}
-	        	
-	        }
-	    }  
-
+		yaoyaoNum = 0;
 		if (window.DeviceMotionEvent){  
 		    window.addEventListener("devicemotion", motionHandler, false);  
 		}
@@ -54,7 +56,14 @@ var MK_Gyroscope = (function () {
 		init: init,
 		rotation: null,
 		yaoyao: null,
-		isIos: isIos
+		isIos: isIos,
+		clear: function(){
+			if (window.DeviceMotionEvent){  
+			    window.removeEventListener("devicemotion", motionHandler, false);  
+			}
+
+			initStatus = false;
+		}
 	};
 
 	return MK_Gyroscope;
